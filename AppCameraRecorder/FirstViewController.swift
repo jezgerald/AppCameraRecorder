995ae11d3cf56ef4e30c8b80625dba686aabd6b1
Jess <jess_uni10@hotmail.com>
2018-05-12 14:48:20 +1000

    Initial Commit

---
+++ AppCameraRecorder/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AppCameraRecorder
+//
+//  Created by Jessica Fitzgerald on 12/5/18.
+//  Copyright © 2018 Jessica Fitzgerald. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
