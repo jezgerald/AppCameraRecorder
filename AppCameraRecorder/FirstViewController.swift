ed59cc2ffe3bcbc99c73d35e2ba6d874e9491cac
Jess <jess_uni10@hotmail.com>
2018-05-12 18:22:24 +1000

    AppCameraRecorder - images, tab controller, video and photo

--- AppCameraRecorder/FirstViewController.swift
+++ AppCameraRecorder/FirstViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
+import Photos
+import AVKit
+import MobileCoreServices // to access camera
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imagePicked: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,58 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // take photo
+    @IBAction func recordButton(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    // open photo library
+    @IBAction func photoLibrary(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary){
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    // save photo to library
+    @IBAction func saveButton(_ sender: UIButton) {
+        let imageData = UIImageJPEGRepresentation(imagePicked.image!, 0)
+        let compressedJPEGImage = UIImage(data: imageData!)
+        UIImageWriteToSavedPhotosAlbum(compressedJPEGImage!, nil, nil, nil)
+        
+        saveNotice()
+    }
+    
+    
+    // TO DO
+    @IBAction func filter(_ sender: UIButton) {
+    }
+    
+    // show photo on main page after taken
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = (info[UIImagePickerControllerEditedImage] ?? info[UIImagePickerControllerOriginalImage]) as? UIImage
+        {
+            imagePicked.image = image
+        }
+        picker.presentingViewController?.dismiss(animated: true);
+    }
+    
+    // alert stating photo has saved
+    func saveNotice() {
+        let alertController = UIAlertController(title: "Photo saved!", message: "Your photo was saved  successfully", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
--- AppCameraRecorder/SecondViewController.swift
+++ AppCameraRecorder/SecondViewController.swift
@@ -7,9 +7,19 @@
 //
 
 import UIKit
+import AVFoundation
+import Photos
+import AVKit
+import MobileCoreServices // to access camera
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
+    
+    // image viewer to show preview of video after taken
+    @IBOutlet weak var videoPicked: UIImageView!
+    
+    var soundRecorder = AVAudioRecorder()
+    var soundPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,131 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // button to record video
+    @IBAction func recordButton(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let mediaUI = UIImagePickerController()
+            mediaUI.delegate = self
+            mediaUI.sourceType = UIImagePickerControllerSourceType.camera
+            mediaUI.mediaTypes = [kUTTypeMovie as String]
+            mediaUI.allowsEditing = true
+            self.present(mediaUI, animated: true, completion: nil)
+        }
+    }
+    
+    // access photo library
+    @IBAction func photoLibrary(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
+            let mediaUI = UIImagePickerController()
+            mediaUI.delegate = self
+            mediaUI.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            mediaUI.mediaTypes = [kUTTypeMovie as String]
+            mediaUI.allowsEditing = true
+            self.present(mediaUI, animated: true, completion: nil)
+        }
+    }
+    
+    // save video to library - working without code???
+    // TO FIX
+    @IBAction func saveButton(_ sender: UIButton) {
+        
+        saveNotice()
+    }
+    
+    // finish and close the video
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
 
+        guard
+            let mediaType = info[UIImagePickerControllerMediaType] as? String,
+            mediaType == (kUTTypeMovie as String),
+            let url = info[UIImagePickerControllerMediaURL] as? URL,
+            UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(url.path)
+            else {
+                return
+        }
+        
+       videoPicked.image = getThumbnailFrom(path: url)
+        
+        // Save video to photo library
+        // Possible to transfer to Save button????? - needs url, info, etc.
+        UISaveVideoAtPathToSavedPhotosAlbum(url.path, self, #selector(video(_:didFinishSavingWithError:contextInfo:)), nil)
+        
+        picker.presentingViewController?.dismiss(animated: true)
+
+    }
+    
+    func getThumbnailFrom(path: URL) -> UIImage? {
+        do {
+            let asset = AVURLAsset(url: path , options: nil)
+            let imgGenerator = AVAssetImageGenerator(asset: asset)
+            imgGenerator.appliesPreferredTrackTransform = true
+            let cgImage = try imgGenerator.copyCGImage(at: CMTimeMake(0, 1), actualTime: nil)
+            let thumbnail = UIImage(cgImage: cgImage)
+            
+            return thumbnail
+        }
+        catch let error {
+            print("*** Error generating thumbnail: \(error.localizedDescription)")
+            return nil
+        }
+    }
+    
+    // error alert if video doesn't work
+    @objc func video(_ videoPath: String, didFinishSavingWithError error: Error?, contextInfo info: AnyObject) {
+        let title = (error == nil) ? "Success" : "Error"
+        let message = (error == nil) ? "Video was saved" : "Video failed to save"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    // currently saving without pressing save
+    // TO FIX
+    func saveNotice() {
+        let alertController = UIAlertController(title: "Video saved!", message: "Your video was successfully saved", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    // TO DO
+    @IBAction func filterButton(_ sender: UIButton) {
+    }
+    
 }
 
+
+/* old code
+
+ --- ImagePickerController function
+ //        let videoType = info[UIImagePickerControllerMediaType] as? String
+ //        videoType == (kUTTypeMovie as String)
+ //        let videoData = AVCaptureFileOutputRecordingDelegate
+ //        let url = info[UIImagePickerControllerMediaURL] as? URL
+ //
+ //        let player = AVPlayer(url: url?)?
+ //        let vcPlayer = AVPlayerViewController()
+ //        vcPlayer.player = player
+ //        self.present(vcPlayer, animated: true, completion: nil)
+ 
+ 
+ //    func fileOutput(AVCaptureOutput, didStartRecordingTo: URL, from: [AVCaptureConnection])
+ //    {
+ //    }
+
+ --- Save button
+ //        let compressedJPEGImage = UIImage(data: imageData!)
+ //        UIImageWriteToSavedPhotosAlbum(compressedJPEGImage!, nil, nil, nil)
+
+ 
+ // (class) AVCaptureFileOutputRecordingDelegate
+ // required for AVCaptureFileOutput:
+ //    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+ //    }
+
+ 
+ */
+ 
