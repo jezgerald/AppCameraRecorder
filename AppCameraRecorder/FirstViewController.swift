f5d14b772dc1fc7bbe563aad01a9d693c1b0f92c
Jess <jess_uni10@hotmail.com>
2018-05-20 21:38:39 +1000

    Commit20May

--- AppCameraRecorder/FirstViewController.swift
+++ AppCameraRecorder/FirstViewController.swift
@@ -8,19 +8,39 @@
 
 import UIKit
 import AVFoundation
-import Photos
+import Photos // to take photos
 import AVKit
 import MobileCoreServices // to access camera
+import CoreImage // to apply filters
 
 class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imagePicked: UIImageView!
+    @IBOutlet weak var slider: UISlider!
+    
+    var currentImage : UIImage!
+    var context : CIContext!
+    var currentFilter : CIFilter!
+    
+    // filter array
+    var CIFilters = [
+        "CIPhotoEffectChrome",
+        "CIPhotoEffectFade",
+        "CIPhotoEffectInstant",
+        "CIPhotoEffectNoir",
+        "CIPhotoEffectProcess",
+        "CIPhotoEffectTonal",
+        "CIPhotoEffectTransfer",
+        "CISepiaTone"
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        context = CIContext()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -57,9 +77,39 @@ class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UI
         saveNotice()
     }
     
-    
-    // TO DO
+    // filter options
     @IBAction func filter(_ sender: UIButton) {
+
+        let ac = UIAlertController(title: "Choose filter", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "CIPhotoEffectChrome", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIPhotoEffectFade", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIPhotoEffectInstant", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIPhotoEffectNoir", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CITwirlDistortion", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIUnsharpMask", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIVignette", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.addAction(UIAlertAction(title: "Reset", style: .destructive, handler: ResetImage))
+        present(ac, animated: true)
+        
+    }
+    
+    func setFilter(action: UIAlertAction) {
+        // make sure we have a valid image before continuing!
+        guard currentImage != nil else { return }
+        
+        currentFilter = CIFilter(name: action.title!)
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        
+        applyProcessing()
+    }
+    
+    func ResetImage (sender: AnyObject) {
+        if let image = self.currentImage {
+            self.imagePicked.image = image
+        }
+        
     }
     
     // show photo on main page after taken
@@ -67,8 +117,42 @@ class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UI
         if let image = (info[UIImagePickerControllerEditedImage] ?? info[UIImagePickerControllerOriginalImage]) as? UIImage
         {
             imagePicked.image = image
+            currentImage = image
         }
-        picker.presentingViewController?.dismiss(animated: true);
+        picker.presentingViewController?.dismiss(animated: true, completion: nil);
+    }
+    
+    // change filter intensity where applicable
+    @IBAction func intensityChanged(_ sender: Any) {
+        applyProcessing()
+    }
+    
+    // process picture with applied filter
+    func applyProcessing() {
+        let inputKeys = currentFilter.inputKeys
+        let intensity = Int(slider.value)
+        
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(intensity, forKey: kCIInputIntensityKey)
+            slider.isHidden = false
+        }
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(intensity * 200, forKey: kCIInputRadiusKey)
+            slider.isHidden = false
+        }
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(intensity * 100, forKey: kCIInputScaleKey)
+            slider.isHidden = false
+        }
+        if inputKeys.contains(kCIInputCenterKey) {
+            currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey)
+            slider.isHidden = false
+        }
+        
+        if let cgimg = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent) {
+            let processedImage = UIImage(cgImage: cgimg)
+            self.imagePicked.image = processedImage
+        }
     }
     
     // alert stating photo has saved
@@ -79,5 +163,64 @@ class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UI
         present(alertController, animated: true, completion: nil)
     }
     
+    
+    // share image to social media or other applications
+    @IBAction func share(_ sender: UIButton) {
+
+        // to choose the image already showing
+        let image = imagePicked.image
+
+        // share options only show if an image has been chosen/taken
+        if image != nil {
+            let activityVC = UIActivityViewController(activityItems: [image as Any], applicationActivities: nil)
+
+            activityVC.popoverPresentationController?.sourceView = sender
+
+            self.present(activityVC, animated: true, completion: nil)
+        }
+        // error message to prompt the user to choose or take a photo
+        else {
+            let alertController = UIAlertController(title: "No photo selected", message: "Choose or take a photo", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(defaultAction)
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    
 }
 
+
+// Old code
+//    @IBAction func applyFilter(sender: AnyObject) {
+//
+//        // Create an image to filter
+//        let inputImage = CIImage(image: imagePicked.image!)
+//
+//        // Create a random color to pass to a filter
+//        let randomColor = [kCIInputAngleKey: (Double(arc4random_uniform(314)) / 100)]
+//
+//        // Apply a filter to the image
+//        let filteredImage = inputImage?.applyingFilter("CIHueAdjust", parameters: randomColor)
+//
+//        // Render the filtered image
+////        let renderedImage = context.createCGImage(filteredImage!, fromRect: filteredImage!.extent())
+//
+//        // Reflect the change back in the interface
+////        imagePicked.image = UIImage(cgImage: renderedImage)
+//
+//    }
+
+//        performSegue(withIdentifier: "photoSegue", sender: self)
+
+// no longer using below as created new View instead of tab
+////FILTER IMAGE TO NEW SEGUE CONTROLLER THING: https://stackoverflow.com/questions/35020242/how-do-i-segue-an-image-to-another-viewcontroller-and-display-it-within-an-image
+//override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//    guard let filterVC = segue.destination as? FilterViewController
+//        else { return }
+//
+//    if segue.identifier == "photoSegue" {
+//        filterVC.selectedImage = imagePicked.image!
+//        //            imagePicked.image = CIImage.applyingFilter(imagePicked)
+//    }
+//}
--- AppCameraRecorder/SecondViewController.swift
+++ AppCameraRecorder/SecondViewController.swift
@@ -8,28 +8,24 @@
 
 import UIKit
 import AVFoundation
-import Photos
 import AVKit
+import Photos // to take photos
 import MobileCoreServices // to access camera
+import CoreImage // to create filters
+import CoreMedia // to create & apply video filters
 
-class SecondViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
+class SecondViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, AVAudioPlayerDelegate, AVAudioRecorderDelegate, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    var captureSession : AVCaptureSession!
     
     // image viewer to show preview of video after taken
     @IBOutlet weak var videoPicked: UIImageView!
     
     var soundRecorder = AVAudioRecorder()
     var soundPlayer = AVAudioPlayer()
+    var videoURL: NSURL?
+    let playerController = AVPlayerViewController()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     // button to record video
     @IBAction func recordButton(_ sender: UIButton) {
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
@@ -38,6 +34,7 @@ class SecondViewController: UIViewController, UIImagePickerControllerDelegate, U
             mediaUI.sourceType = UIImagePickerControllerSourceType.camera
             mediaUI.mediaTypes = [kUTTypeMovie as String]
             mediaUI.allowsEditing = true
+            mediaUI.showsCameraControls = true
             self.present(mediaUI, animated: true, completion: nil)
         }
     }
@@ -54,12 +51,11 @@ class SecondViewController: UIViewController, UIImagePickerControllerDelegate, U
         }
     }
     
-    // save video to library - working without code???
-    // TO FIX
+    // save video to library
     @IBAction func saveButton(_ sender: UIButton) {
         
-        saveNotice()
     }
+    
     
     // finish and close the video
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
@@ -69,20 +65,67 @@ class SecondViewController: UIViewController, UIImagePickerControllerDelegate, U
             mediaType == (kUTTypeMovie as String),
             let url = info[UIImagePickerControllerMediaURL] as? URL,
             UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(url.path)
-            else {
-                return
-        }
+            else { return }
         
-       videoPicked.image = getThumbnailFrom(path: url)
+        // trying to extract URL to be used elsewhere
+//        self.videoURL = url as NSURL
         
-        // Save video to photo library
-        // Possible to transfer to Save button????? - needs url, info, etc.
-        UISaveVideoAtPathToSavedPhotosAlbum(url.path, self, #selector(video(_:didFinishSavingWithError:contextInfo:)), nil)
+        // trying to create a way to view the video on the main screen before saving
+        let player = AVPlayer(url: url)
+        let vcPlayer = AVPlayerViewController()
+        vcPlayer.player = player
+        vcPlayer.showsPlaybackControls = true
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.bounds
+        self.view.layer.addSublayer(playerLayer)
+        self.present(vcPlayer, animated: true, completion: nil)
+        player.play()
         
+        // dismisses view controller
         picker.presentingViewController?.dismiss(animated: true)
-
+        
+        // saves video to Photo Library
+        UISaveVideoAtPathToSavedPhotosAlbum(url.path, self, #selector(video(_:didFinishSavingWithError:contextInfo:)), nil)
+        saveNotice()
     }
     
+    // trying to add code to play the video
+    @IBAction func playButton(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: "videoPlaySegue", sender: self)
+//
+////        if let videoURL = videoURL {
+//        let player = AVPlayer(url: videoURL! as URL)
+//        let vcPlayer = AVPlayerViewController()
+//        vcPlayer.player = player
+//        self.present(vcPlayer, animated: true, completion: nil)
+//        }
+//        else {
+//            let alertController = UIAlertController(title: "No video selected", message: "Choose or take a video", preferredStyle: .alert)
+//            let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+//            alertController.addAction(defaultAction)
+//            present(alertController, animated: true, completion: nil)
+//        }
+    }
+
+    func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let destination = segue.destination as!
+        AVPlayerViewController
+        let url = self.videoURL
+        destination.player = AVPlayer(url: url! as URL)
+    }
+    
+    // video function required to save video
+    @objc func video(_ videoPath: String, didFinishSavingWithError error: Error?, contextInfo info: AnyObject) {
+        let title = (error == nil) ? "Success" : "Error"
+        let message = (error == nil) ? "Video was saved" : "Video failed to save"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // get thumbnail of video to show on main screen
     func getThumbnailFrom(path: URL) -> UIImage? {
         do {
             let asset = AVURLAsset(url: path , options: nil)
@@ -99,29 +142,66 @@ class SecondViewController: UIViewController, UIImagePickerControllerDelegate, U
         }
     }
     
-    // error alert if video doesn't work
-    @objc func video(_ videoPath: String, didFinishSavingWithError error: Error?, contextInfo info: AnyObject) {
-        let title = (error == nil) ? "Success" : "Error"
-        let message = (error == nil) ? "Video was saved" : "Video failed to save"
-        
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil))
-        present(alert, animated: true, completion: nil)
-    }
-    
-    
-    // currently saving without pressing save
-    // TO FIX
+    // alert to inform user that their video has saved to their photo library
     func saveNotice() {
         let alertController = UIAlertController(title: "Video saved!", message: "Your video was successfully saved", preferredStyle: .alert)
         let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
         alertController.addAction(defaultAction)
         present(alertController, animated: true, completion: nil)
     }
+
+    // error message to prompt the user to choose or take a video
+    func errorNotice() {
+        
+        let alertController = UIAlertController(title: "No video selected", message: "Choose or take a video", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true, completion: nil)
+    }
     
-    
-    // TO DO
+    // filter options to apply to video
     @IBAction func filterButton(_ sender: UIButton) {
+        
+    }
+    
+    
+    // share video
+    @IBAction func share(_ sender: UIButton) {
+        
+        // to choose the image already showing
+        let video = videoPicked.image
+        
+        // share options only show if an image has been chosen/taken
+        if video != nil {
+            let activityVC = UIActivityViewController(activityItems: [video as Any], applicationActivities: nil)
+            
+            activityVC.popoverPresentationController?.sourceView = sender
+            
+            self.present(activityVC, animated: true, completion: nil)
+        }
+            // error message to prompt the user to choose or take a photo
+        else {
+            let alertController = UIAlertController(title: "No video selected", message: "Choose or take a video", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(defaultAction)
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
     }
     
 }
